3511d62c25099f77de310c86d2b84c7143fdcf16
Davi Diorio Mendes <d.mendes@sidi.org.br>
2021-03-23 15:45:11 -0300

    Kill all the mutants

--- src/mutant-detector-service.js
+++ src/mutant-detector-service.js
@@ -3,6 +3,11 @@ const _ = require('lodash')
 const HUMAN_MAX_BRAIN_ACTIVITY = 10000.0
 const INVALID_PERSON = 'Invalid person value';
 
+/**
+ * Evaluate brain activity for a list of persons.
+ * @param {List<Person{name, brain_activity}>} persons Persons to evaluate brain activity
+ * @returns {List<Person{name, diagnostic}>} Diagnostic for persons
+ */
 function evaluateList(persons) {
     const result = [];
     for (person of persons) {
@@ -11,9 +16,14 @@ function evaluateList(persons) {
     return result;
 }
 
+/**
+ * Evaluate brain activity for a person.
+ * @param {Person{name, brain_activity}} person Person to evaluate brain activity
+ * @returns Person{name, diagnostic} Diagnostic for person
+ */
 function evaluatePerson(person) {
     if (!_.has(person, 'name') || !_.has(person, 'brain_activity')) {
-        throw INVALID_PERSON;
+        throw new Error(INVALID_PERSON);
     }
 
     return {
@@ -22,6 +32,11 @@ function evaluatePerson(person) {
     };
 }
 
+/**
+ * Evaluate if brain activity level is compatible with a human brain activity.
+ * @param {float} brainActivity Brain activity value
+ * @returns true if it's human brain activity. false if it's mutant brain activity
+ */
 function isHuman(brainActivity) {
     return brainActivity <= HUMAN_MAX_BRAIN_ACTIVITY;
 }
--- test/mutant-detector-service.spec.js
+++ test/mutant-detector-service.spec.js
@@ -1,39 +1,91 @@
+const _ = require('lodash')
 const assert = require('assert');
 const MutantDetector = require('../src/mutant-detector-service')
 
 describe('Mutant Detector Test', () => {
 
-    it('evaluateList should not throw when evaluating valid data', () => {
-        let persons = [
-            {
-                'name': 'Dr. Xavier',
-                'brain_activity': 50000
-            },
-            {
-                'name': 'Logan',
-                'brain_activity': 30000
-            },
-            {
-                'name': 'William Stryker',
-                'brain_activity': 10000
-            }
-        ];
-        assert.doesNotThrow(() => MutantDetector.evaluateList(persons))
+    const INVALID_PERSON = new Error('Invalid person value');
+    const personsBase = {
+        xavier: {
+            name: 'Dr. Xavier',
+            brain_activity: 50000,
+            diagnostic: 'mutant'
+        },
+        logan: {
+            name: 'Logan',
+            brain_activity: 30000,
+            diagnostic: 'mutant'
+        },
+        stryker: {
+            name: 'William Stryker',
+            brain_activity: 10000,
+            diagnostic: 'human'
+        }
+    };
+
+    describe('isHuman', () => {
+        it('Should return true for maximum human brain activity', () => {
+            assert.ok(MutantDetector.isHuman(10000.0));
+        });
+
+        it('Should return true for brain activity below maximum human brain activity', () => {
+            assert.ok(MutantDetector.isHuman(9999.9));
+        });
+
+        it('Should return false for brain activity above maximun human brain activity', () => {
+            assert.ok(!MutantDetector.isHuman(10000.1));
+        });
     });
 
-    it('evaluateList should throw error for invalid person format', () => {
-        let brokenPersons = [
-            {
-                'name': 'Dr. Xavier',
-            },
-            {
-                'brain_activity': 30000
-            },
-            {
-                'name': 'William Stryker',
-                'brain_activity': 10000
-            }
-        ];
-        assert.throws(() => MutantDetector.evaluateList(brokenPersons));
+    describe('evaluatePerson', () => {
+        it('Should return Person{name, diagnostic: "mutant"} for a mutant', () => {
+            let person = personsBase.xavier; // mutant
+            let result = MutantDetector.evaluatePerson(person);
+            assert.ok(_.has(result, 'name'));
+            assert.strictEqual(result['name'], person.name);
+            assert.ok(_.has(result, 'diagnostic'));
+            assert.strictEqual(result['diagnostic'], person.diagnostic);
+        });
+
+        it('Should return Person{name, diagnostic: "human"} for a human', () => {
+            let person = personsBase.stryker; // human
+            let result = MutantDetector.evaluatePerson(person);
+            assert.ok(_.has(result, 'name'));
+            assert.strictEqual(result['name'], person.name);
+            assert.ok(_.has(result, 'diagnostic'));
+            assert.strictEqual(result['diagnostic'], person.diagnostic);
+        });
+
+        it('Should throw "Invalid person value" when missing name', () => {
+            let person = _.omit(
+                personsBase.logan,
+                'name'
+            );
+
+            assert.throws(
+                () => MutantDetector.evaluatePerson(person),
+                INVALID_PERSON
+            );
+        });
+
+        it('Should throw "Invalid person value" when missing brain_activity', () => {
+            let person = _.omit(
+                personsBase.logan,
+                'brain_activity'
+            );
+
+            assert.throws(
+                () => MutantDetector.evaluatePerson(person),
+                INVALID_PERSON
+            );
+        });
+    });
+
+    describe('evaluateList', () => {
+        it('Result length must be the same as persons lenght', () => {
+            let persons = _.values(personsBase);
+            let result = MutantDetector.evaluateList(persons);
+            assert.strictEqual(result.length, persons.length);
+        });
     });
 });
